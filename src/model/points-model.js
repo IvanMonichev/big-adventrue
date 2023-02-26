cc19ca61b3c3b1304d8e8bdb62f29dcbe368138e
Ivan Monichev <ivan@monichev.ru>
2023-02-26 22:42:11 +0300

    feat: add adapters

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -8,7 +8,9 @@ export default class PointsModel extends Observable {
   constructor(taskApiService) {
     super();
     this.#taskApiService = taskApiService;
-    this.#taskApiService.points.then((points) => console.log(points));
+    this.#taskApiService.points.then((points) => {
+      console.log(points.map(this.#adaptToClient));
+    });
 
   }
 
@@ -43,6 +45,23 @@ export default class PointsModel extends Observable {
 
     this.#points = [...this.#points.slice(0, index), ...this.#points.slice(index + 1)];
     this._notify(updateType, update);
+  };
+
+  #adaptToClient = (point) => {
+    const adaptedPoint = {...point,
+      basePrice: point['base_price'] !== null ? new Date(point['base_price']) : point['base_price'],
+      dateFrom: point ['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point ['date_to'],
+      isFavorite: point['is_favorite'],
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
   };
 }
 
--- src/services/points-api-service.js
+++ src/services/points-api-service.js
@@ -16,12 +16,29 @@ export default class PointsApiService extends ApiService {
     const response = await this._load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'})
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  };
+
+  #adaptToServer = (point) => {
+    const adaptedPoint = {...point,
+      'date_from': point.dateFrom,
+      'base_price': point.basePrice,
+      'date_to': point.dateTo,
+      'is_favorite': point.isFavorite,
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.dateTo;
+    delete adaptedPoint.isFavorite;
+
+    return adaptedPoint;
   };
 }
